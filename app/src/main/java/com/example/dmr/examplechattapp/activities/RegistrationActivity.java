50a5844bbd0fe1c3d97db0c12d4d34c5a6187628
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-13 09:33:40 +0500

    Registration layout done

---
+++ app/src/main/java/com/example/dmr/examplechattapp/activities/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.dmr.examplechattapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dmr.examplechattapp.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
