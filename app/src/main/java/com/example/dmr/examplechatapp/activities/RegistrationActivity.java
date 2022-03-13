132e36762b9b25371da408e5a053718b8187819c
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-13 12:51:07 +0500

    Registration layout initialized

--- app/src/main/java/com/example/dmr/examplechatapp/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/dmr/examplechatapp/activities/RegistrationActivity.java
@@ -4,13 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.dmr.examplechatapp.R;
+import com.example.dmr.examplechattapp.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    TextInputEditText name, address, phone, city, email, pass, cPass;
+    MaterialButton signup;
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        init();
+
+    }
+    void init(){
+        name=findViewById(R.id.full_name);
+        address=findViewById(R.id.address);
+        phone=findViewById(R.id.contact);
+        city=findViewById(R.id.city);
+        email=findViewById(R.id.email);
+        pass=findViewById(R.id.password);
+        cPass=findViewById(R.id.confirm_password);
+        signup=findViewById(R.id.signup);
+        firestore=FirebaseFirestore.getInstance();
+        auth=FirebaseAuth.getInstance();
     }
 }
