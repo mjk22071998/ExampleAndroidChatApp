e9650460aad5f11d3f38a8cba55517b4af010db1
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-13 14:07:42 +0500

    Registration complete

--- app/src/main/java/com/example/dmr/examplechatapp/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/dmr/examplechatapp/activities/RegistrationActivity.java
@@ -1,15 +1,25 @@
 package com.example.dmr.examplechatapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.dmr.examplechattapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -24,7 +34,14 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         init();
-
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()){
+                    signUpUser();
+                }
+            }
+        });
     }
     void init(){
         name=findViewById(R.id.full_name);
@@ -75,5 +92,40 @@ public class RegistrationActivity extends AppCompatActivity {
         } else {
             return true;
         }
+    }
+
+    void signUpUser(){
+        auth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Map<String,Object> map = new HashMap<>();
+                map.put("Name",name.getText().toString());
+                map.put("Contact No.", phone.getText().toString());
+                map.put("Address",address.getText().toString());
+                map.put("City",city.getText().toString());
+                map.put("email",email.getText().toString());
+                map.put("password",pass.getText().toString());
+                firestore.collection("Users").document(email.getText().toString()).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Intent intent=new Intent(RegistrationActivity.this,MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegistrationActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
     }
 }
