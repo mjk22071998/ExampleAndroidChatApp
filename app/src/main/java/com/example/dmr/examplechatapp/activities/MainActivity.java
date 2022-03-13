cc8e687b08411141cfcae533966949971c4ab3a7
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-13 20:47:53 +0500

    MainActivity complete

--- app/src/main/java/com/example/dmr/examplechatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/dmr/examplechatapp/activities/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.dmr.examplechatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.dmr.examplechatapp.R;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=findViewById(R.id.login);
+        signup=findViewById(R.id.signup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+            }
+        });
     }
 }
